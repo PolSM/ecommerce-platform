18e41239fccaf29a02ad06a65f21961874053317
Pol <polsm95@gmail.com>
2024-09-29 03:01:51 +0200

    feat: setup H2 database

--- src/main/java/org/example/infrastructure/PriceController.java
+++ src/main/java/org/example/infrastructure/PriceController.java
@@ -6,12 +6,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/prices")
+@RequestMapping("/price")
 public class PriceController {
     @GetMapping
-    public String getPrices(@RequestParam("application_date") String applicationDate,
-                            @RequestParam("product_id") String productId,
-                            @RequestParam("brand_id") String brandId) {
-        return "List of prices";
+    public String getPrice(
+            @RequestParam("product_id") String productId,
+            @RequestParam("brand_id") String brandId,
+            @RequestParam("date") String date
+    ) {
+        return "a price";
     }
 }
